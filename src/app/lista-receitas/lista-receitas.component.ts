1d194be0c86b05f5b22d7d05b840d5cadb0aa57f
Cainã Jose <josecainan21@gmail.com>
2025-05-03 18:21:29 -0300

    Filtro de Receita por emoção

--- src/app/lista-receitas/lista-receitas.component.ts
+++ src/app/lista-receitas/lista-receitas.component.ts
@@ -2,29 +2,41 @@ import { Component, OnInit } from '@angular/core';
 import { ReceitasService } from '../services/receitas.service';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from "../navbar/navbar.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-lista-receitas',
   standalone: true, // 👈 isso aqui é obrigatório
-  imports: [CommonModule, NavbarComponent], // 👈 importa CommonModule aqui também
+  imports: [CommonModule,FormsModule, NavbarComponent], // 👈 importa CommonModule aqui também
   templateUrl: './lista-receitas.component.html',
   styleUrl:'./lista-receitas.component.css'
 })
 export class ListaReceitasComponent implements OnInit {
   receitas: any[] = [];
+  emocaoSelecionada: string = '';
+  emocoesDisponiveis: string[] = [];
+
 
   constructor(private receitasService: ReceitasService) {}
 
   ngOnInit(): void {
     this.receitasService.getReceitas().subscribe({
       next: (dados) => {
-        //console.log('✅ Receitas recebidas:', dados);
         this.receitas = Object.values(dados);
-        console.log(this.receitas[0])
+        this.emocoesDisponiveis = [...new Set(
+          this.receitas.flatMap(r => r.sentimentoReceita || [])
+        )];
       },
       error: (erro) => {
         console.error('❌ Erro ao buscar receitas:', erro);
       }
     });
   }
+  getReceitasFiltradas(): any[] {
+    if (!this.emocaoSelecionada) return this.receitas;
+    return this.receitas.filter(r =>
+      r.sentimentoReceita?.includes(this.emocaoSelecionada)
+    );
+  }
+    
 }
